2e1f00dfb7f4e9aa4addeba9832d80540ceb4a0c
meekhamyno <78329436+meekhamyno@users.noreply.github.com>
2024-10-28 12:16:34 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
-  output: 'standalone',
+  distDir: 'build'
 };
 
 export default nextConfig;
